507b3816cdc86d3328634854bbd18efd60f3a5b0
skifox52 <fet.a@hotmail.fr>
2023-02-21 16:02:44 +0100

    MVP2

--- routes/offreRoute.js
+++ routes/offreRoute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const {
   getAllOffres,
   postOffer,
@@ -13,12 +12,5 @@ offreRouter
   .post("/add", protectRecruteur, postOffer)
   .delete("/:id", protectRecruteur, deleteOffre)
   .put("/:id", protectRecruteur, updateOffer)
-=======
-const { getAllOffres, postOffer,deleteOffre,updateOffer,getCandidatOffres} = require("../controllers/offreController")
-
-const offreRouter = require("express").Router()
-
-offreRouter.get("/all", getAllOffres).post("/add", postOffer).delete("/:id",deleteOffre).put("/:id",updateOffer).get("/:candidatId",getCandidatOffres )
->>>>>>> d0db468d249a6eabf278e8f1388f23b9d217e642
 
 module.exports = offreRouter
